9419881f42713398841b841015545163e3eba2ee
Link Dupont <link@sub-pop.net>
2024-01-24 12:58:47 -0500

    apply gofmt styling

--- main.go
+++ main.go
@@ -1,75 +1,76 @@
 package main
 
 import (
-    "fmt"
-    "io/fs"
-    "os"
-    "strings"
-    "github.com/xeipuuv/gojsonschema"
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
+
+	"github.com/xeipuuv/gojsonschema"
 )
 
 var validationErrors []string
 
 func main() {
-    dirs, _ := os.ReadDir("./")
+	dirs, _ := os.ReadDir("./")
 
-    for _, dir := range dirs {
-        validate(dir)
-    }
+	for _, dir := range dirs {
+		validate(dir)
+	}
 
-    outputResults()
+	outputResults()
 }
 
 func validate(dir fs.DirEntry) {
-    dirName := dir.Name()
-    if dir.IsDir() && stringContains(gatewayDirNames(), dirName) {
-        fmt.Println("Running schema validation against sample " + dirName + " identities")
-        schemaLoader := gojsonschema.NewReferenceLoader("file://" + dirName + "/schema.json")
+	dirName := dir.Name()
+	if dir.IsDir() && stringContains(gatewayDirNames(), dirName) {
+		fmt.Println("Running schema validation against sample " + dirName + " identities")
+		schemaLoader := gojsonschema.NewReferenceLoader("file://" + dirName + "/schema.json")
 
-        files, _ := os.ReadDir(dirName + "/identities")
-        for _, file := range files {
-            fileName := file.Name()
-            if !file.IsDir() {
-                documentLoader := gojsonschema.NewReferenceLoader("file://" + dirName + "/identities/" + fileName)
+		files, _ := os.ReadDir(dirName + "/identities")
+		for _, file := range files {
+			fileName := file.Name()
+			if !file.IsDir() {
+				documentLoader := gojsonschema.NewReferenceLoader("file://" + dirName + "/identities/" + fileName)
 
-                result, err := gojsonschema.Validate(schemaLoader, documentLoader)
-                if err != nil {
-                    panic(err.Error())
-                }
+				result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+				if err != nil {
+					panic(err.Error())
+				}
 
-                if !result.Valid() {
-                    for _, desc := range result.Errors() {
-                        e := "- " + dirName + "/" + fileName + ": " + desc.Description()
-                        validationErrors = append(validationErrors, e)
-                    }
-                }
-            }
-        }
-    }
+				if !result.Valid() {
+					for _, desc := range result.Errors() {
+						e := "- " + dirName + "/" + fileName + ": " + desc.Description()
+						validationErrors = append(validationErrors, e)
+					}
+				}
+			}
+		}
+	}
 }
 
 func stringContains(slice []string, ele string) bool {
-    for _, sliceEle := range slice {
-        if sliceEle == ele {
-            return true
-        }
-    }
+	for _, sliceEle := range slice {
+		if sliceEle == ele {
+			return true
+		}
+	}
 
-    return false
+	return false
 }
 
 func gatewayDirNames() []string {
-    return []string{"3scale", "turnpike"}
+	return []string{"3scale", "turnpike"}
 }
 
 func outputResults() {
-    fmt.Println()
+	fmt.Println()
 
-    if len(validationErrors) > 0 {
-        fmt.Printf("Error(s) found in validation:\n")
-        fmt.Println(strings.Join(validationErrors, "\n"))
-        os.Exit(1)
-    } else {
-        fmt.Printf("No errors found.\n")
-    }
+	if len(validationErrors) > 0 {
+		fmt.Printf("Error(s) found in validation:\n")
+		fmt.Println(strings.Join(validationErrors, "\n"))
+		os.Exit(1)
+	} else {
+		fmt.Printf("No errors found.\n")
+	}
 }
