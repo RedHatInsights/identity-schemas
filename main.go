ba93c49d3409434e345ff3b02e36b2cdacfad13a
Link Dupont <link@sub-pop.net>
2024-01-24 13:57:00 -0500

    refactor output
    validation errors are kept in a map with file basename as a key. When
    the output is printed, the -format flag can be used to determine the
    output format (json or text)

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
+	"text/tabwriter"
 
+	"github.com/sgreben/flagvar"
 	"github.com/xeipuuv/gojsonschema"
 )
 
-var validationErrors []string
+var validationErrors map[string][]string
 
 var (
-	schemaFile string
+	schemaFile   string
+	outputFormat flagvar.Enum
 )
 
 func main() {
+	validationErrors = make(map[string][]string)
+	outputFormat.Choices = []string{"text", "json"}
+	outputFormat.Value = "text"
+
 	flag.StringVar(&schemaFile, "schema", "./schema.json", "load `FILE` as the schema when validating")
+	flag.Var(&outputFormat, "format", "output in `FORMAT` (json, text)")
 
 	flag.Parse()
 
@@ -46,13 +54,32 @@ func main() {
 			if err != nil {
 				log.Fatalf("cannot validate document: %v", err)
 			}
-			for _, e := range errors {
-				validationErrors = append(validationErrors, fmt.Sprintf("%v: %v", filepath.Base(file), e))
-			}
+			validationErrors[filepath.Base(file)] = make([]string, 0)
+			validationErrors[filepath.Base(file)] = append(validationErrors[filepath.Base(file)], errors...)
 		}
 	}
 
-	outputResults()
+	switch outputFormat.Value {
+	case "json":
+		data, err := json.Marshal(validationErrors)
+		if err != nil {
+			log.Fatalf("cannot marshal json: %v", err)
+		}
+		fmt.Println(string(data))
+	case "text":
+		writer := tabwriter.NewWriter(os.Stdout, 4, 4, 2, ' ', 0)
+		fmt.Fprint(writer, "FILE\tERROR\n")
+		for file, errors := range validationErrors {
+			for _, err := range errors {
+				fmt.Fprintf(writer, "%v\t%v\n", file, err)
+			}
+		}
+		if err := writer.Flush(); err != nil {
+			log.Fatalf("unable to flush tab writer: %v", err)
+		}
+	default:
+		log.Fatalf("unknown format type: %v", outputFormat.Value)
+	}
 }
 
 // validateDocument runs JSON schema validation using the given loader on the
@@ -71,16 +98,4 @@ func validateDocument(loader gojsonschema.JSONLoader, document string) (bool, []
 		return false, errors, nil
 	}
 	return true, nil, nil
-}
-
-func outputResults() {
-	fmt.Println()
-
-	if len(validationErrors) > 0 {
-		fmt.Printf("Error(s) found in validation:\n")
-		fmt.Println(strings.Join(validationErrors, "\n"))
-		os.Exit(1)
-	} else {
-		fmt.Printf("No errors found.\n")
-	}
 }
